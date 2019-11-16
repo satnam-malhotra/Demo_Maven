403c2c8aba33323a2d90b8008eeda9ff29125601
Satnam Malhotra <satnammalhotra@Satnams-MacBook-Air.local>
2019-11-16 14:01:02 +0530

    First commit

---
+++ src/test/java/com/demo/project/AbstractChromeWebDriverTest.java
@@ -0,0 +1,41 @@
+
+package com.demo.project;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AbstractChromeWebDriverTest {
+
+	protected WebDriver driver;
+
+	public AbstractChromeWebDriverTest() {
+		super();
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		//Download the web driver executable
+		WebDriverManager.chromedriver().setup();
+		
+		//Create a instance of your web driver - chrome
+		driver = new ChromeDriver();
+		driver.get("http://www.gmail.com");
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+	
+	public void sleep(int seconds) {
+		try {
+			Thread.sleep(seconds * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
